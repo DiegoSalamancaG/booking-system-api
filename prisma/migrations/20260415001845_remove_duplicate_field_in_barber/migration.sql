112ff994d5c72c0d0e0b567c17086c72f09db5cc
Diego <diegosalamanca.guajardo@gmail.com>
2026-04-14 20:24:35 -0400

    feat: remove duplicated audit field in barber model

---
+++ prisma/migrations/20260415001845_remove_duplicate_field_in_barber/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated_at` on the `barbers` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "barbers" DROP COLUMN "updated_at";
